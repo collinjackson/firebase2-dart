f08bf86bb367eca34e38f3e90dc1e5f900767b80
Collin Jackson <jackson@google.com>
2016-05-04 17:35:22 -0700

    Remove unused files

--- lib/firebase_js.dart
+++
@@ -1,8 +0,0 @@
-library firebase;
-
-export 'src/data_snapshot.dart';
-export 'src/disconnect.dart';
-export 'src/encode.dart';
-export 'src/event.dart';
-export 'src/js/firebase.dart';
-export 'src/transaction_result.dart';
